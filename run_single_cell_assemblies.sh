296ed2054ec8d083e2d3502e7d8b28660382b2fa
Guy Leonard <guy.leonard@gmail.com>
2016-04-18 12:18:43 +0100

    tidied up some of the code/comments

--- run_single_cell_assemblies.sh
+++ run_single_cell_assemblies.sh
@@ -2,39 +2,28 @@
 # Guy Leonard MMXVI
 
 ##
-# This is just a suggested workflow, it works on our servers...you will have
-# to adapt it to your location.
+# This is a suggested workflow, it works on our servers/data...YMMV
+##
 
-## This script uses CEGMA - notoriously difficult to install and now unsupported
-# You might not want to use it, you are safe to comment it out
+##
+# User Variables
+##
 
-## It also uses BUSCO
-# I still don't trust BUSCO hence also using CEGMA
-# If you see something like this:
-# Error: Sequence file ./run_testing//augustus_proteins/64334.fas is empty or misformatted
-# just ignore it, BUSCO is 'working' it's an error from hmmer and there are no gene predictions
-# for the SCOs
-
-# Number of Cores to Use
+# Number of processor cores
 THREADS=8
-
-## Change these to your server's directory locations
 
 ## NCBI Databases
 # NCBI 'nt' Database Location
 NCBI_NT=/storage/ncbi/nt/nt
-# NCBI Taxonomy
+# NCBI Taxonomy Location
 NCBI_TAX=/storage/ncbi/taxdump
 
 ## CEGMA Environment Variables
 # CEGMA DIR
 export CEGMA=/home/cs02gl/programs/CEGMA_v2
 export PERL5LIB=$PERL5LIB:/home/cs02gl/programs/CEGMA_v2/lib
-# This is a pretty important one not to forget
-# else you get this error
-# FATAL ERROR when running local map 6400: "No such file or directory"
 export WISECONFIGDIR=/usr/share/wise/
-## Ammend PATH for CEGMA bin
+# Ammend PATH for CEGMA bin
 export PATH=$PATH:/home/cs02gl/programs/CEGMA_v2/bin
 
 ## BUSCO Environment Variables
@@ -43,39 +32,33 @@ BUSCO_DB=/storage/databases/BUSCO/eukaryota
 # Augustus Config Path
 export AUGUSTUS_CONFIG_PATH=~/programs/augustus-3.0.2/config/
 
-## Locations of binaries, if not in path
-# and tests to make sure they can be called
-#PIGz
-command -v pigz >/dev/null 2>&1 || { echo "I require pigz but it's not installed.  Aborting." >&2; exit 1;}
-#TRIM_GALORE
-command -v trim_galore >/dev/null 2>&1 || { echo "I require Trim Galore! but it's not installed.  Aborting." >&2; exit 1;}
-#PEAR
-command -v pear >/dev/null 2>&1 || { echo "I require PEAR but it's not installed.  Aborting." >&2; exit 1;}
-#SPAdes
+## Locations of binaries if not in path
+#SPADES
 SPADES=/home/cs02gl/programs/SPAdes-3.7.0-Linux/bin/
-command -v $SPADES/spades.py >/dev/null 2>&1 || { echo "I require SPAdes but it's not installed.  Aborting." >&2; exit 1;}
 #QUAST
 QUAST=/home/cs02gl/programs/quast-3.2
-command -v $QUAST/quast.py >/dev/null 2>&1 || { echo "I require QUAST but it's not installed.  Aborting." >&2; exit 1;}
 #CEGMA
 CEGMA_DIR=/home/cs02gl/programs/CEGMA_v2/bin
-command -v $CEGMA_DIR/cegma >/dev/null 2>&1 || { echo "I require CEGMA but it's not installed.  Aborting." >&2; exit 1;}
 #BUSCO
 BUSCO=/home/cs02gl/programs/BUSCO_v1.1b1
-command -v $BUSCO/BUSCO_v1.1b1.py >/dev/null 2>&1 || { echo "I require BUSCO but it's not installed.  Aborting." >&2; exit 1;}
-#BWA
-command -v bwa >/dev/null 2>&1 || { echo "I require bwa but it's not installed.  Aborting." >&2; exit 1;}
-#SAMTOOLS1.3
-command -v samtools1.3 >/dev/null 2>&1 || { echo "I require Samtools 1.3 but it's not installed.  Aborting." >&2; exit 1;}
-#BLASTN
-command -v blastn >/dev/null 2>&1 || { echo "I require BLASTn but it's not installed.  Aborting." >&2; exit 1;}
 #BLOBTOOLS
 BLOBTOOLS=/home/cs02gl/programs/blobtools
+
+## Check programs are executable
+command -v pigz >/dev/null 2>&1 || { echo "I require pigz but it's not installed.  Aborting." >&2; exit 1;}
+command -v trim_galore >/dev/null 2>&1 || { echo "I require Trim Galore! but it's not installed.  Aborting." >&2; exit 1;}
+command -v pear >/dev/null 2>&1 || { echo "I require PEAR but it's not installed.  Aborting." >&2; exit 1;}
+command -v $SPADES/spades.py >/dev/null 2>&1 || { echo "I require SPAdes but it's not installed.  Aborting." >&2; exit 1;}
+command -v $QUAST/quast.py >/dev/null 2>&1 || { echo "I require QUAST but it's not installed.  Aborting." >&2; exit 1;}
+command -v $CEGMA_DIR/cegma >/dev/null 2>&1 || { echo "I require CEGMA but it's not installed.  Aborting." >&2; exit 1;}
+command -v $BUSCO/BUSCO_v1.1b1.py >/dev/null 2>&1 || { echo "I require BUSCO but it's not installed.  Aborting." >&2; exit 1;}
+command -v bwa >/dev/null 2>&1 || { echo "I require bwa but it's not installed.  Aborting." >&2; exit 1;}
+command -v samtools1.3 >/dev/null 2>&1 || { echo "I require Samtools 1.3 but it's not installed.  Aborting." >&2; exit 1;}
+command -v blastn >/dev/null 2>&1 || { echo "I require BLASTn but it's not installed.  Aborting." >&2; exit 1;}
 command -v $BLOBTOOLS/blobtools >/dev/null 2>&1 || { echo "I require BLOBTOOLS but it's not installed.  Aborting." >&2; exit 1;}
-#MultiQC
 command -v multiqc >/dev/null 2>&1 || { echo "I require MultiQC but it's not installed.  Aborting." >&2; exit 1;}
 
-## Try not change below here...
+## Try not to change code below here...
 # Working Directory
 WD=`pwd`
 echo "$WD"
