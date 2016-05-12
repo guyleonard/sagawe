2863de8adf95c74687da3e4736b7903b1e2a3eec
Guy Leonard <guy.leonard@gmail.com>
2016-05-12 16:19:17 +0100

    minor update: lib- libncurses5-dev needed for samtools

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,7 +15,7 @@ update_path () {
 
 # Update Dependencies
 sudo apt-get update -q
-sudo apt-get install -y -q build-essential autoconf automake libtool python-setuptools python-dev python-pip pigz unzip default-jdk default-jre libfreetype6-dev libpng-dev pkg-config
+sudo apt-get install -y -q build-essential autoconf automake libtool python-setuptools python-dev python-pip pigz unzip default-jdk default-jre libfreetype6-dev libpng-dev pkg-config libncurses5-dev
 
 # Make an install location
 if [ ! -d 'build' ]; then
