eacb50fb5f3b3e0f7bef3032bef4396db268e774
Guy Leonard <guy.leonard@gmail.com>
2016-11-10 14:37:48 +0000

    reworking

--- mitochondria_analysis/extract_mapped_mito_reads.sh
+++ mitochondria_analysis/extract_mapped_mito_reads.sh
@@ -2,31 +2,30 @@
 
 for dir in *; do
     if [[ -d $dir ]]; then
-       echo "${dir}"
-       cd $dir
+       echo "Working in ${dir}"
+       cd ${dir}
 
        # remove old/previous files
-       echo "Removing .fastq"
-       rm *.fastq
-       rm *.fq
+       echo "Removing old .fastq"
+       #rm *.fastq
+       #rm *.fq
 
        # remove other old junk files
-       echo "Removing junk files"
-       rm "${dir}_mt_mapped_unassembled_paired_reads.qsort.bam"
-       rm "${dir}_mt_mapped_all_reads.bam"
-       rm "${dir}_mt_mapped_all_reads.bam.bai"
+       #echo "Removing junk files"
+       #rm "${dir}_mapped_all_reads.bam"
+       #rm "${dir}_mapped_all_reads.bam.bai"
 
 	# samtools index the bam files
 	echo "Indexing *.bam files"
-	samtools index "${dir}_mt_mapped_assembled_reads.bam"
-	samtools index "${dir}_mt_mapped_unassembled_paired_reads.bam"
-	samtools index "${dir}_mt_mapped_unassembled_unpaired_forward_reads.bam"
-	samtools index "${dir}_mt_mapped_unassembled_unpaired_reverse_reads.bam"
+	samtools index "${dir}_mapped_assembled_reads.bam"
+	samtools index "${dir}_mapped_unassembled_paired_reads.bam"
+	samtools index "${dir}_mapped_unassembled_unpaired_forward_reads.bam"
+	samtools index "${dir}_mapped_unassembled_unpaired_reverse_reads.bam"
 
 	# get the scaffolds from the fasta file
         # and extract the reads from the bame
 	echo "Extract Reads for each Scaffold"
-	grep ">" ${dir}_mt.fa | while read -r line ; do
+	grep ">" ${dir}.fa | while read -r line ; do
 
 		# by the power of magic pattern substitution!
 		scaffold=${line/>/}
@@ -34,10 +33,10 @@ for dir in *; do
 		node="${node_array[1]}_${node_array[2]}"
 
 		echo "Extracting reads that match to ${node}"
-		samtools view -bh ${dir}_mt_mapped_assembled_reads.bam ${scaffold} > ${node}_mapped_assembled_reads.bam
-		samtools view -bh ${dir}_mt_mapped_unassembled_paired_reads.bam ${scaffold} > ${node}_mapped_unassembled_paired_reads.bam
-		samtools view -bh ${dir}_mt_mapped_unassembled_unpaired_forward_reads.bam ${scaffold} > ${node}_mapped_unassembled_unpaired_forward_reads.bam
-		samtools view -bh ${dir}_mt_mapped_unassembled_unpaired_reverse_reads.bam ${scaffold} > ${node}_mapped_unassembled_unpaired_reverse_reads.bam
+		samtools view -bh ${dir}_mapped_assembled_reads.bam ${scaffold} > ${node}_mapped_assembled_reads.bam
+		samtools view -bh ${dir}_mapped_unassembled_paired_reads.bam ${scaffold} > ${node}_mapped_unassembled_paired_reads.bam
+		samtools view -bh ${dir}_mapped_unassembled_unpaired_forward_reads.bam ${scaffold} > ${node}_mapped_unassembled_unpaired_forward_reads.bam
+		samtools view -bh ${dir}_mapped_unassembled_unpaired_reverse_reads.bam ${scaffold} > ${node}_mapped_unassembled_unpaired_reverse_reads.bam
 
 		echo "Converting extracted bams to fastq"
 		bamtools convert -format fastq -in ${node}_mapped_assembled_reads.bam > ${node}_mapped_assembled_reads.fq
@@ -58,15 +57,19 @@ for dir in *; do
 	split-paired-reads.py -0 orphans ${dir}_unassembled_paired_unordered_interleaved_reads.fastq
 	# append the orphans to one of the single reads files
 	cat orphans >> ${dir}_unassembled_unpaired_reverse_reads.fastq
-	rm orphans
+	#rm orphans
 	# re-inter-leave/lace the properly paired reads
-	interleave-reads.py ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.1 ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.2 -o ${dir}_unassembled_paired_unordered_interleaved_reads.fastq
+	interleave-reads.py ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.1 ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.2 -o ${dir}_unassembled_paired_ordered_interleaved_reads.fastq
 
-	mkdir node_fq
+	#rm ${dir}_unassembled_paired_unordered_interleaved_reads.fastq
+	#rm ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.1
+	#rm ${dir}_unassembled_paired_unordered_interleaved_reads.fastq.2
+
+	mkdir -p node_fq
 	mv *.fq node_fq
 
-	mkdir node_bam
-	mv NODE*.bam node_bam
+	mkdir -p node_bam
+	mv NODE*.bam* node_bam
 
 	cd ../
     fi
