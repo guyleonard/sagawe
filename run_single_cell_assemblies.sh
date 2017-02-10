6f360b60eee2591dd1e23f2d547dfca260959738
Guy Leonard <guy.leonard@gmail.com>
2017-02-10 16:19:03 +0000

    more modular - still needs work and testing

--- run_single_cell_assemblies.sh
+++ run_single_cell_assemblies.sh
@@ -62,169 +62,6 @@ while getopts f:r:o:pth FLAG; do
 done
 
 
-## Try not to change code below here...
-# Working Directory
-#WD=$(pwd)
-#echo "$WD"
-
-# Get filenames for current Single Cell Library
-# Locations of FASTQs = Sample_**_***/raw_illumina_reads/
-for DIRS in */ ; do
-	#echo "Working in $DIRS"
-	#current_dir=$WD/$DIRS/raw_illumina_reads
-	
-	# GZIP FASTQs
-	# saving space down the line, all other files will be gzipped
-	#echo "gzipping *.fastq files"
-	#time pigz -9 -R ./*.fastq
-
-	# Get all fastq.gz files
-	#FASTQ=(*.fastq.gz)
-
-    # Lets GZIP these!
-    #echo "gzipping fastq files"
-    #pigz -9 -R $WD/$DIRS/raw_illumina_reads/PEAR/*.fastq
-
-
-	# Run BlobTools
-	mkdir -p BLOBTOOLS
-	cd BLOBTOOLS
-	mkdir -p MAPPING
-	cd MAPPING
-	# index assembly (scaffolds.fa) with BWA
-	echo "Indexing Assembly"
-	bwa index -a bwtsw $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta | tee bwa.log
-
-	# map reads to assembly with BWA MEM
-	# we will have to do this for all 5 sets of reads and then merge
-	echo "Mapping Assembled reads to Assembly"
-	bwa mem -t $THREADS $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	$WD/$DIRS/raw_illumina_reads/PEAR/pear_overlap.assembled_trimmed.fq.gz \
-	> $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_assembled_reads.sam | tee -a bwa.log
-
-        echo "Mapping Un-assembled & Un-Paired reads to Assembly - Forward"
-        bwa mem -t $THREADS $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	$WD/$DIRS/raw_illumina_reads/PEAR/pear_overlap.unassembled.forward_unpaired_1.fq.gz \
-        > $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_forward_reads.sam | tee -a bwa.log
-
-	echo "Mapping Un-assembled & Un-Paired reads to Assembly - Reverse"
-        bwa mem -t $THREADS $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	$WD/$DIRS/raw_illumina_reads/PEAR/pear_overlap.unassembled.reverse_unpaired_2.fq.gz \
-        > $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_reverse_reads.sam | tee -a bwa.log
-
-        echo "Mapping Un-assembled but still Paired reads to Assembly"
-        bwa mem -t $THREADS $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	$WD/$DIRS/raw_illumina_reads/PEAR/pear_overlap.unassembled.forward_val_1.fq.gz \
-	$WD/$DIRS/raw_illumina_reads/PEAR/pear_overlap.unassembled.reverse_val_2.fq.gz \
-        > $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_paired_reads.sam | tee -a bwa.log
-
-        # sort and convert sam to bam with SAMTOOLS
-        echo "Sorting Assembled SAM File and Converting to BAM"
-        samtools sort -@ $THREADS -o $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_assembled_reads.bam \
-        $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_assembled_reads.sam | tee -a samtools.log
-
-        # sort and convert sam to bam with SAMTOOLS
-        echo "Sorting Un-assembled & Un-Paired SAM Files and Converting to BAM - Forward"
-        samtools sort -@ $THREADS -o $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_forward_reads.bam \
-        $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_forward_reads.sam | tee -a samtools.log
-
-        echo "Sorting Un-assembled & Un-Paired SAM Files and Converting to BAM - Reverse"
-        samtools sort -@ $THREADS -o $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_reverse_reads.bam \
-        $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_reverse_reads.sam | tee -a samtools.log
-
-        # sort and convert sam to bam with SAMTOOLS
-        echo "Sorting Un-assembled but still Paired SAM File and Converting to BAM"
-        samtools sort -@ $THREADS -o $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_paired_reads.bam \
-        $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_paired_reads.sam | tee -a samtools.log
-
-	# Merge SAM files
-	echo "Merging 4 BAM files"
-	samtools merge -@ $THREADS -f $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_all_reads.bam \
-	$WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_assembled_reads.bam \
-	$WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_paired_reads.bam \
-	$WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_forward_reads.bam \
-	$WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_unassembled_unpaired_reverse_reads.bam | tee -a samtools.log
-
-	echo "Indexing Bam"
-	samtools index $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_all_reads.bam | tee -a samtools.log
-
-	if [ ! -f $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_all_reads.bam.bai ] ; then
-		echo -e "[ERROR]\t[$DIRS]: No index file was created for your BAM file. !?" >> $WD/$DIRS/raw_illumina_reads/errors.txt
-		# blobtools create will crash without this file, so we might as well move on to the next library...
-		#break
-		#continue
-	fi
-
-	# delete sam file - save some disk space, we have the bam now
-	rm $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/*.sam
-	cd ../
-
-	# run blast against NCBI 'nt'
-	mkdir -p BLAST
-	cd BLAST
-	echo "Running BLAST"
-	blastn -task megablast \
-	-query $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	-db $NCBI_NT \
-	-evalue 1e-10 \
-	-num_threads $THREADS \
-	-outfmt '6 qseqid staxids bitscore std sscinames sskingdoms stitle' \
-	-culling_limit 5 \
-	-out $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/BLAST/scaffolds_vs_nt_1e-10.megablast | tee blast.log
-	cd ../
-
-	# run blobtools create
-	echo "Running BlobTools CREATE - slow"
-	cd $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/
-	blobtools create -i $WD/$DIRS/raw_illumina_reads/SPADES/overlapped_and_paired/scaffolds.fasta \
-	--nodes $NCBI_TAX/nodes.dmp --names $NCBI_TAX/names.dmp \
-	-t $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/BLAST/scaffolds_vs_nt_1e-10.megablast \
-	-b $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/MAPPING/scaffolds_mapped_all_reads.bam \
-	-o scaffolds_mapped_reads_nt_1e-10_megablast_blobtools | tee -a $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/blobtools.log
-
-	if  [ ! -f $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json ] ; then
-		echo -e "[ERROR]\t[$DIRS]: Missing blobtools JSON, no tables or figures produced." >> $WD/$DIRS/raw_illumina_reads/errors.txt
-	else
-		# run blobtools view - table output
-		# Standard Output - Phylum
-		echo "Running BlobTools View"
-		blobtools view -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
-		--out $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools_phylum_table.csv | tee -a blobtools.log
-		# Other Output - Species
-		blobtools view -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
-		--out $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools_superkingdom_table.csv \
-		--rank superkingdom | tee -a blobtools.log
-
-		# run blobtools plot - image output
-		# Standard Output - Phylum, 7 Taxa
-		echo "Running BlobTools Plots - Standard + SVG"
-		blobtools plot -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json
-		blobtools plot -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
-		--format svg | tee -a blobtools.log
-
-		# Other Output - Species, 15 Taxa
-		echo "Running BlobTools Plots - SuperKingdom + SVG"
-		blobtools plot -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
-		-r superkingdom
-		blobtools plot -i $WD/$DIRS/raw_illumina_reads/BLOBTOOLS/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
-		-r superkingdom \
-		--format svg | tee -a blobtools.log
-	fi
-
-	# Run MultiQC for some extra, nice stats reports on QC etc
-	cd ../
-        multiqc $WD/$DIRS/raw_illumina_reads/
-
-	# Finish up.
-	cd ../../
-	echo "`pwd`"
-	echo "Complete Run, Next or Finish."
-
-	# end if from scaffolds.fasta check
-	fi
-done
-
-
 #######################
 ## Program Functions ##
 #######################
@@ -329,27 +166,125 @@ function report_busco () {
 }
 
 function report_multiqc () {
-
+	multiqc $current_dir
 }
 
 function blobtools_bwa () {
+	ln -s $assembly_dir/overlapped_and_paired/scaffolds.fasta $blobtools_map/scaffolds.fasta
 
+	# index assembly (scaffolds.fa) with BWA
+	echo "Indexing Assembly"
+	bwa index -a bwtsw $blobtools_map/scaffolds.fasta | tee $blobtools_map/bwa.log
+
+	# map reads to assembly with BWA MEM
+	# we will have to do this for all 5 sets of reads and then merge
+	echo "Mapping Assembled reads to Assembly"
+	bwa mem -t $THREADS $blobtools_map/scaffolds.fasta \
+	$trimmed_dir/assembled_trimmed.fq.gz \
+	> $blobtools_map/scaffolds_mapped_assembled_reads.sam | tee -a $blobtools_map/bwa.log
+
+    echo "Mapping Un-assembled & Un-Paired reads to Assembly - Forward"
+    bwa mem -t $THREADS $blobtools_map/scaffolds.fasta \
+	$trimmed_dir/unassembled.forward_unpaired_1.fq.gz \
+    > $blobtools_map/scaffolds_mapped_unassembled_unpaired_forward_reads.sam | tee -a $blobtools_map/bwa.log
+
+	echo "Mapping Un-assembled & Un-Paired reads to Assembly - Reverse"
+    bwa mem -t $THREADS $blobtools_map/scaffolds.fasta \
+	$trimmed_dir/unassembled.reverse_unpaired_2.fq.gz \
+    > $blobtools_map/scaffolds_mapped_unassembled_unpaired_reverse_reads.sam | tee -a $blobtools_map/bwa.log
+
+    echo "Mapping Un-assembled but still Paired reads to Assembly"
+    bwa mem -t $THREADS $blobtools_map/scaffolds.fasta \
+	$trimmed_dir/unassembled.forward_val_1.fq.gz \
+	$trimmed_dir/unassembled.reverse_val_2.fq.gz \
+    > $blobtools_map/scaffolds_mapped_unassembled_paired_reads.sam | tee -a $blobtools_map/bwa.log
+}
+
+function blobtools_samtools () {
+    # sort and convert sam to bam with SAMTOOLS
+    echo "Sorting Assembled SAM File and Converting to BAM"
+    samtools sort -@ $THREADS -o $blobtools_map/scaffolds_mapped_assembled_reads.bam \
+    $blobtools_map/scaffolds_mapped_assembled_reads.sam | tee -a $blobtools_map/samtools.log
+
+    # sort and convert sam to bam with SAMTOOLS
+    echo "Sorting Un-assembled & Un-Paired SAM Files and Converting to BAM - Forward"
+    samtools sort -@ $THREADS -o $blobtools_map/scaffolds_mapped_unassembled_unpaired_forward_reads.bam \
+    $blobtools_map/scaffolds_mapped_unassembled_unpaired_forward_reads.sam | tee -a $blobtools_map/samtools.log
+
+    echo "Sorting Un-assembled & Un-Paired SAM Files and Converting to BAM - Reverse"
+    samtools sort -@ $THREADS -o $blobtools_map/scaffolds_mapped_unassembled_unpaired_reverse_reads.bam \
+    $blobtools_map/scaffolds_mapped_unassembled_unpaired_reverse_reads.sam | tee -a $blobtools_map/samtools.log
+
+    # sort and convert sam to bam with SAMTOOLS
+    echo "Sorting Un-assembled & Paired SAM File and Converting to BAM"
+    samtools sort -@ $THREADS -o $blobtools_map/scaffolds_mapped_unassembled_paired_reads.bam \
+    $blobtools_map/scaffolds_mapped_unassembled_paired_reads.sam | tee -a $blobtools_map/samtools.log
+
+	# Merge SAM files
+	echo "Merging 4 BAM files"
+	samtools merge -@ $THREADS -f $blobtools_map/scaffolds_mapped_all_reads.bam \
+	$blobtools_map/scaffolds_mapped_assembled_reads.bam \
+	$blobtools_map/scaffolds_mapped_unassembled_paired_reads.bam \
+	$blobtools_map/scaffolds_mapped_unassembled_unpaired_forward_reads.bam \
+	$blobtools_map/scaffolds_mapped_unassembled_unpaired_reverse_reads.bam | tee -a $blobtools_map/samtools.log
+
+	echo "Indexing Bam"
+	samtools index $blobtools_map/scaffolds_mapped_all_reads.bam | tee -a $blobtools_map/samtools.log
+
+	# delete sam files
+	rm $blobtools_map/*.sam
 }
 
 function blobtools_blast () {
-
+	echo "Running BLAST"
+	blastn -task megablast \
+	-query $blobtools_map/scaffolds.fasta \
+	-db $NCBI_NT \
+	-evalue 1e-10 \
+	-num_threads $THREADS \
+	-outfmt '6 qseqid staxids bitscore std sscinames sskingdoms stitle' \
+	-culling_limit 5 \
+	-out $blobtools_blast/scaffolds_vs_nt_1e-10.megablast | tee $blobtools_blast/blast.log
 }
 
 function blobtools_create () {
-
+	echo "Running BlobTools CREATE - slow"
+	blobtools create -i $blobtools_map/scaffolds.fasta \
+	--nodes $NCBI_TAX/nodes.dmp --names $NCBI_TAX/names.dmp \
+	-t $blobtools_blast/scaffolds_vs_nt_1e-10.megablast \
+	-b $blobtools_map/scaffolds_mapped_all_reads.bam \
+	-o $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools | tee -a $blobtools_dir/blobtools.log
 }
 
 function blobtools_table () {
-
+	# Standard Output - Phylum
+	echo "Running BlobTools View"
+	blobtools view -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
+	--out $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools_phylum_table.csv | tee -a $blobtools_dir/blobtools.log
+	
+	# Other Output - Species
+	blobtools view -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
+	--out $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools_superkingdom_table.csv \
+	--rank superkingdom | tee -a $blobtools_dir/blobtools.log
 }
 
+# run blobtools plot - image output
 function blobtools_image () {
+	# Standard Output - Phylum, 7 Taxa
+	echo "Running BlobTools Plots - Standard + SVG"
+	blobtools plot -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json | tee -a $blobtools_dir/blobtools.log
 
+	blobtools plot -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
+	--format svg | tee -a $blobtools_dir/blobtools.log
+
+
+	# Other Output - Species, 15 Taxa
+	echo "Running BlobTools Plots - SuperKingdom + SVG"
+	blobtools plot -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
+	-r superkingdom | tee -a $blobtools_dir/blobtools.log
+	
+	blobtools plot -i $blobtools_dir/scaffolds_mapped_reads_nt_1e-10_megablast_blobtools.BlobDB.json \
+	-r superkingdom --format svg | tee -a $blobtools_dir/blobtools.log
 }
 
 
@@ -385,10 +320,11 @@ function HELP {
 }
 
 
+###########################
 ## Main Pipeline Actions ##
+###########################
 
 THREADS=$(cores)
-echo "num_threads:$THREADS"
 
 # Check that we have the required programs
 exes=('pigz' 'clumpify.sh' 'trim_galore' 'pear' 'spades.py' 'quast.py' 'cegma' 'BUSCO_v1.22.py' 'bwa' 'samtools' 'blastn' 'blobtools' 'multiqc')
